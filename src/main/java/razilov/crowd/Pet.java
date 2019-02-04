89effd60c13092f53c8b7a941d68739ebd88485f
nickraz <nickraz@yandex.ru>
2019-02-04 22:32:30 +0300

    Pets renamed to Pet. Constructor Pet added

---
+++ src/main/java/razilov/crowd/Pet.java
@@ -0,0 +1,11 @@
+package razilov.crowd;
+
+public class Pet {
+    private String name;
+    private int age;
+
+    public Pet(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
--- src/main/java/razilov/crowd/Pets.java
+++
@@ -1,6 +0,0 @@
-package razilov.crowd;
-
-public class Pets {
-    private String name;
-    private int age;
-}
