276b05def3e71164b89548b63a3a08afadf326c9
nickraz <nickraz@yandex.ru>
2019-02-04 22:37:49 +0300

    Just pull it

--- src/main/java/razilov/crowd/CrowdApplication.java
+++ src/main/java/razilov/crowd/CrowdApplication.java
@@ -1,10 +1,7 @@
 package razilov.crowd;
 
-import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.persistence.Entity;
 
 @SpringBootApplication
 public class CrowdApplication {
